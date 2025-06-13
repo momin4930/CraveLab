3710ea19069f3ff2141d2fa6eba345838b0563c3
Sanaullah Momin <sanaullahmomin111@gmail.com>
2025-06-13 12:45:10 +0500

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/CraveLab",
+  base:"/CraveLab/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
